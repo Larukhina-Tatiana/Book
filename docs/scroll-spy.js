73c7f6a0d0a48c3ccf4fc413e7292998e0846bed
Татьяна <ltasya72@gmail.com>
2026-05-29 19:31:29 +0300

    добавлен scroll-spy.js

---
+++ docs/scroll-spy.js
@@ -0,0 +1,32 @@
+// scrol-spy.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll("article"); // всі секції-картки
+  console.log(sections);
+  const navLinks = document.querySelectorAll(".nav__link"); // всі лінки меню
+  console.log(navLinks);
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // зняти підсвічування з усіх лінків
+          navLinks.forEach((link) => link.classList.remove("active"));
+
+          // знайти лінк, який відповідає видимій секції
+          const activeLink = document.querySelector(
+            `.nav__link[href="#${entry.target.id}"]`,
+          );
+          if (activeLink) {
+            activeLink.classList.add("active");
+          }
+        }
+      });
+    },
+    {
+      threshold: 0.4, // секція вважається активною, якщо видно 40% її висоти
+    },
+  );
+
+  // спостерігаємо за кожною секцією
+  sections.forEach((section) => observer.observe(section));
+});
